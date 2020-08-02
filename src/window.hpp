b0cfc781eb5698d17fbf7f7529b3d134739c0910
Daniel <daniel_g_hansen@hotmail.com>
2020-08-02 18:19:59 +0200

    Now we run too!

--- src/window.hpp
+++ src/window.hpp
@@ -9,9 +9,9 @@
 
 #include <string>
 
-#include "imgui/imgui.h"
-#include "imgui/imgui_impl_glfw.h"
-#include "imgui/imgui_impl_opengl3.h"
+#include <imgui/imgui.h>
+#include <imgui/examples/imgui_impl_glfw.h>
+#include <imgui/examples/imgui_impl_opengl3.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
